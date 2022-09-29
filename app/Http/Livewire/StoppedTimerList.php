95dc75eacd622d2b83e2b4e01c6805618fa907e7
Michael Lefrancois <michael@lefrancois.de>
2022-09-29 14:45:07 +0200

    filters text, date and deleted filter

--- app/Http/Livewire/StoppedTimerList.php
+++ app/Http/Livewire/StoppedTimerList.php
@@ -2,14 +2,51 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Livewire\Component;
 use App\Models\Tracker as TrackerModel;
 
 class StoppedTimerList extends Component
 {
     public TrackerModel $tracker;
+    public $filterDate;
+    public $filterText;
+    public Bool $filterDeleted = false;
+    public $items;
 
     protected $listeners = ['refreshTracker' => '$refresh'];
+
+    public function mount() {
+        $this->filter();
+    }
+
+    public function filter() {
+        $this->items = $this->tracker->items;
+        if ($this->filterDate) {
+            $this->items = $this->items->whereBetween('start', [Carbon::parse($this->filterDate)->startOfDay(), Carbon::parse($this->filterDate)->endOfDay()]);
+        }
+        if ($this->filterDeleted) {
+            $this->items = $this->items->where('deleted', false);
+        }
+        if ($this->filterText) {
+            $this->items = $this->items->filter(function ($i) {
+                return Str::contains($i->title, $this->filterText) || Str::contains($i->notes, $this->filterText);
+            });
+        }
+    }
+
+    public function updatedFilterDate() {
+        $this->filter();
+    }
+
+    public function updatedFilterText() {
+        $this->filter();
+    }
+
+    public function updatedFilterDeleted() {
+        $this->filter();
+    }
 
     public function render()
     {
--- resources/views/livewire/stopped-timer-list.blade.php
+++ resources/views/livewire/stopped-timer-list.blade.php
@@ -1,10 +1,38 @@
 <div>
-{{--        <div class="flex justify-between items-center" v-if="stoppedTimers.length > 0">--}}
-{{--            <div class="relative">--}}
-{{--                <div class="flex items-center">--}}
-{{--                    <date-picker :date="selectedDate" :markDates="allDates" enableReset="true" @set-date="setSelectedDate"/>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+    @if ($tracker->items->count())
+        <div class="flex justify-between items-center space-x-4">
+            <div class="mb-3 w-full">
+                <div class="text-xs font-bold mb-1">
+                    {{ __('Search for ...') }}
+                </div>
+                <div class="flex">
+                    <input type="text" wire:model.debounce="filterText" placeholder="{{ __('Searchstring') }}" class="h-10 shadow-sm w-full ml-0 items-center flex focus:ring-cosu-500 focus:border-cosu-500 block w-full sm:text-sm border-gray-300 rounded-l-md">
+                    <button class="bg-cosu-900 text-white rounded-r-md px-3 text-xs whitespace-nowrap" wire:click="$set('filterText', null)">
+                        {{ __('Reset filter') }}
+                    </button>
+                </div>
+            </div>
+            <div class="mb-3 w-full">
+                <div class="text-xs font-bold mb-1">
+                    {{ __('Date filter') }}
+                </div>
+                <div class="flex">
+                    <input type="date" wire:model.debounce="filterDate" class="h-10 shadow-sm w-full ml-0 items-center flex focus:ring-cosu-500 focus:border-cosu-500 block w-full sm:text-sm border-gray-300 rounded-l-md">
+                    <button class="bg-cosu-900 text-white rounded-r-md px-3 text-xs whitespace-nowrap" wire:click="$set('filterDate', null)">
+                        {{ __('Reset filter') }}
+                    </button>
+                </div>
+            </div>
+            <div class="mb-3 w-full text-right">
+                <div class="text-xs font-bold mb-1">
+                    {{ __('Filter deleted') }}
+                </div>
+                <div class="">
+                    <input type="checkbox" wire:model.debounce="filterDeleted" class="appearance-none h-10 shadow-sm w-10 border-gray-300 checked:bg-cosu-900 checked:hover:bg-cosu-900 rounded-md">
+                </div>
+            </div>
+        </div>
+    @endif
 {{--            <div class="relative">--}}
 {{--                <div @click="toggleExport = !toggleExport" class="bg-cyan-500 shadow p-2 w-32 text-center font-medium rounded-md cursor-pointer flex justify-center items-center hover:bg-cyan-600 text-white h-8">--}}
 {{--                    <div class="">--}}
@@ -23,16 +51,15 @@
 {{--                    </div>--}}
 {{--                </div>--}}
 {{--            </div>--}}
-{{--        </div>--}}
 
         <div class="bg-white shadow overflow-hidden sm:rounded-md">
 
-            @if ($tracker->items->count())
+            @if ($items->count())
                 <ul class="divide-y divide-gray-200">
                     @php
                         $lastDate = '';
                     @endphp
-                    @foreach($tracker->items->whereNotNull('end')->sortByDesc('start') as $timer)
+                    @foreach($items->whereNotNull('end')->sortByDesc('start') as $timer)
                         @if ($lastDate != $timer->start->format('d.m.Y'))
                             <div class="text-xs py-1 px-3 text-center bg-cosu-900 text-white">
                                 {{ $timer->start->format('d.m.Y') }}
