b6f7fd9944e490acb24d3626b68f53923b547991
Michael Lefrancois <michael@lefrancois.de>
2022-10-07 13:45:46 +0200

    fathom integration

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,15 +1,23 @@
-<footer>
-    <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-        <div class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left">
-            <div class="flex justify-center flex-col lg:flex items-center">
-                <div class="w-full text-center">
-                    <span class="">&copy; {{ \Carbon\Carbon::now()->format('Y') }} COSU</span>
-                    <span class=""> All rights reserved.</span>
-                </div>
-                <div class="w-full mt-4 flex justify-center">
-                    <a class="block sm:inline hover:text-gray-700 mx-2" href="#" target="_blank">{{ __('Imprint') }}</a>
-                    <a class="block sm:inline hover:text-gray-700 mx-2" href="#" target="_blank">{{ __('Privacy policy') }}</a>
-                </div>
+<footer class="container mx-auto py-6">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-sm text-gray-500">
+        <div class="flex flex-col lg:flex-row justify-between space-y-2 lg:space-y-0">
+            <div class="text-center lg:text-left">
+                <span class="">&copy; {{ \Carbon\Carbon::now()->format('Y') }} COSU</span>
+                <span class=""> All rights reserved.</span>
+            </div>
+            <div class="text-center hidden lg:block">
+                <p>
+                    <a href="https://usefathom.com/ref/ABAKKC" target="_blank" class="text-xs">We ❤️ beautiful, simple website analytics by Fathom</a>
+                </p>
+            </div>
+            <div class="text-center lg:text-left flex lg:block justify-center">
+                <a class="block sm:inline hover:text-gray-700 mx-2" href="#" target="_blank">{{ __('Imprint') }}</a>
+                <a class="block sm:inline hover:text-gray-700 mx-2" href="#" target="_blank">{{ __('Privacy policy') }}</a>
+            </div>
+            <div class="text-center lg:hidden">
+                <p>
+                    <a href="https://usefathom.com/ref/ABAKKC" target="_blank" class="text-xs">We ❤️ beautiful, simple website analytics by Fathom</a>
+                </p>
             </div>
         </div>
     </div>
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -263,53 +263,6 @@
             </div>
         </div>
     </div>
-    <!-- <div class="bg-gray-50 pt-12 sm:pt-16">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="max-w-4xl mx-auto text-center">
-            <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-                Used by developers all over the world
-            </h2>
-            <p class="mt-3 text-xl text-gray-500 sm:mt-4">
-                We
-            </p>
-        </div>
-    </div>
-    <div class="mt-10 pb-12 bg-white sm:pb-16">
-        <div class="relative">
-            <div class="absolute inset-0 h-1/2 bg-gray-50"></div>
-            <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div class="max-w-4xl mx-auto">
-                    <dl class="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
-                        <div class="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
-                            <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-                                Trackers
-                              </dt>
-                            <dd class="order-1 text-5xl font-extrabold text-cosu-600">
-                               {{ \Carbon\Carbon::now()->format('d.m.Y') }}
-    </dd>
-</div>
-<div class="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
-    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-        Timers
-    </dt>
-    <dd class="order-1 text-5xl font-extrabold text-cosu-600">
-        24/7
-    </dd>
-</div>
-<div class="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
-    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">
-        Coffees
-    </dt>
-    <dd class="order-1 text-5xl font-extrabold text-cosu-600">
-        100k
-    </dd>
-</div>
-</dl>
-</div>
-</div>
-</div>
-</div>
-</div> -->
     <div class="bg-white-200">
         <div class="bg-cosu-900">
             <div class="pt-12 px-4 sm:px-6 lg:px-8 lg:pt-20">
