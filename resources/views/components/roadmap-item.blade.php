80131e00adabfb23abaad95ee3e20a234b498e27
Michael Lefrancois <michael@lefrancois.de>
2022-10-07 16:05:13 +0200

    roadmap

--- resources/views/components/roadmap-item.blade.php
+++ resources/views/components/roadmap-item.blade.php
@@ -1,7 +1,8 @@
 @props([
     'icon' => 'add',
     'end' => false,
-    'color' => 'blue'
+    'color' => 'blue',
+    'done' => false,
 ])
 <li>
     <div class="relative pb-8">
@@ -18,17 +19,18 @@
             </div>
             <div class="min-w-0 flex-1 py-0">
                 <div class="text-sm leading-8 text-gray-500">
-                                    <span class="mr-0.5">
-                                        <a href="#" class="relative inline-flex items-center rounded-full border border-gray-300 px-3 py-0.5 text-sm">
-                                          <span class="absolute flex flex-shrink-0 items-center justify-center">
-                                              <span class="hidden bg-red-500 bg-yellow-500 bg-blue-500"></span>
-                                              <span class="h-1.5 w-1.5 rounded-full bg-{{ $color }}-500" aria-hidden="true"></span>
-                                          </span>
-                                          <span class="ml-3.5 font-medium text-gray-900">
-                                              {{ $slot }}
-                                          </span>
-                                        </a>
-                                    </span>
+                    <span class="mr-0.5">
+                        <a href="#" class="relative inline-flex items-center rounded-full border border-gray-300 px-3 py-0.5 text-sm {{ $done ? 'bg-cosu-500 text-white' : '' }}">
+                            <span class="font-medium">
+                                {{ $slot }}
+                            </span>
+                            @if ($done)
+                                <span class="flex flex-shrink-0 items-center justify-center ml-2">
+                                    <i class="ri-check-fill text-white"></i>
+                              </span>
+                            @endif
+                        </a>
+                    </span>
                 </div>
             </div>
         </div>
