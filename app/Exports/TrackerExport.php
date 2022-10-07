89ba300d13512ceca3d6cf730688b93e6ba0d637
Michael Lefrancois <michael@lefrancois.de>
2022-10-07 15:39:05 +0200

    exports

---
+++ app/Exports/TrackerExport.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Timer;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+
+class TrackerExport implements FromCollection, WithHeadings, WithMapping
+{
+    protected String $trackerId;
+
+    public function __construct(String $trackerId) {
+        $this->trackerId = $trackerId;
+    }
+
+    public function map($timer): array
+    {
+        return [
+            $timer->title,
+            $timer->notes,
+            $timer->start,
+            $timer->end,
+            $timer->duration,
+            $timer->edited,
+            $timer->manual,
+            $timer->deleted,
+        ];
+    }
+
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return Timer::where('tracker_id', $this->trackerId)->orderBy('start')->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Title',
+            'Notes',
+            'Start',
+            'End',
+            'Duration',
+            'Edited?',
+            'Manual?',
+            'Deleted?',
+        ];
+    }
+}
--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,23 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tracker;
-use Illuminate\Http\Request;
 use Ulid\Ulid;
 
 class MainController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('frontend.index');
     }
 
-    public function create() {
+    public function create()
+    {
         $tracker = Tracker::create([
             'identifier' => Ulid::generate(true),
         ]);
         return redirect()->route('tracker', ['id' => $tracker->identifier]);
     }
 
-    public function tracker(String $id) {
+    public function tracker(string $id)
+    {
         return view('frontend.tracker', [
             'identifier' => $id,
         ]);
