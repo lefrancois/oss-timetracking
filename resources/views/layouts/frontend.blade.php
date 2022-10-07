7971b102c9e5c5398afa812e559568c737b4eee5
Michael Lefrancois <michael@lefrancois.de>
2022-10-07 16:15:06 +0200

    favicon and meta infos

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -3,7 +3,22 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" type="image/png" href="{{ asset('/favicon.png') }}">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="{{ __('Easy to use and integrate time tracking for everybody. No restrictions, no strings attached.') }}">
+        <meta property="og:title" content="{{ config('app.name', 'Laravel') }}" />
+        <meta property="og:description" content="{{ __('Easy to use and integrate time tracking for everybody. No restrictions, no strings attached.') }}" />
+        <meta property="og:site_name" content="{{ config('app.name', 'Laravel') }}" />
+        <meta property="og:image" content="{{ url(asset('assets/images/hero-preview.png')) }}" />
+        <meta property="og:url" content="{{ URL::current() }}" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:title" content="{{ config('app.name', 'Laravel') }}" />
+        <meta name="twitter:description" content="{{ __('Easy to use and integrate time tracking for everybody. No restrictions, no strings attached.') }}" />
+        <meta name="twitter:url" content="{{ URL::current() }}" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:image" content="{{ url(asset('assets/images/hero-preview.png')) }}" />
+        <link rel="canonical" href="{{ URL::current() }}"/>
+        <meta name="robots" content="index,follow" />
         <title>{{ config('app.name', 'Laravel') }}</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
